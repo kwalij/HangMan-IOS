0426c33dce782738205212e794418ad237b92649
Kazim Walji <kwalji@bsd220.org>
2019-10-21 10:20:24 -0500

    Initial Commit

---
+++ HangMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HangMan
+//
+//  Created by Kazim Walji on 10/21/19.
+//  Copyright © 2019 Mahdi Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
